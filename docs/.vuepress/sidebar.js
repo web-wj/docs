dd52a50073882424f4e0213d0ce805420efe875b
Alex Wang <frontend_alex@126.com>
2022-01-20 00:05:45 +0800

    frontend/docs: init the sidebar party

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,32 +1,70 @@
 module.exports = {
-  '/language/chinese/': [
-    //Group1
-    {
-        title: 'Group1',
-        children: [
-            {
-                title: 'chinese',   // 必要的
-                children: ['']
-            }
-        ]
-    },
-    //Group2
-    {
-        title: 'Group2',
-        children: [
-            //小组A
-            {
-                title: 'A',   
-                children: ['a'],
-            },
-            //小组B
-            {
-                title: 'B',   
-                children: ['b'],
-            },
-        ],
-    },
-  ],
-}
-
-
+	"/frontend/": [
+		{
+			title: "基础",
+			children: [
+				{
+					title: "h5",
+				},
+				{
+					title: "css3",
+				},
+			],
+		},
+		{
+			title: "轮子",
+			children: [
+				{
+					title: "A",
+				},
+				{
+					title: "B",
+				},
+			],
+		},
+		{
+			title: "小程序",
+			children: [
+				{
+					title: "微信小程序",
+				},
+				{
+					title: "uniapp",
+				},
+			],
+		},
+	],
+	"/backend/": [
+		{
+			title: "后端",
+			children: [
+				{
+					title: "node",
+				},
+			],
+		},
+	],
+	"/interview/": [
+		{
+			title: "面试问题",
+			children: [
+				{
+					title: "git",
+				},
+			],
+		},
+	],
+	"/projects/": [
+		{
+			title: "项目经验",
+			children: [
+				{
+					title: "vue",
+				},
+				{
+					title: "ng1",
+				},
+			],
+		},
+	],
+};
--- docs/README.md
+++ docs/README.md
@@ -4,5 +4,5 @@ heroImage: /images/programmer.gif
 heroText: 笔记本
 tagline: 路漫漫其修远兮,吾将上下而求索。
 actionText: 开始翻阅  ☞
-actionLink: /vue/
+actionLink: /frontend/base/h5/
 ---
