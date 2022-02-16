e6b789e4ac526b819ea368b93b378bf75fa0994d
Alex Wang <frontend_alex@126.com>
2022-02-17 00:12:21 +0800

    frontend/docs: write the file about the base nodejs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,5 +5,13 @@ module.exports = {
     logo: '/images/favicon.png',
     nav: require('./nav.js'),
     sidebar: require('./sidebar.js')
-  }
+  },
+  markdown: {
+    // 为代码块添加行数
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/active-header-links', {
+    sidebarLinkSelector: '.sidebar-link',
+    headerAnchorSelector: '.header-anchor'
+  }]
 }
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -36,12 +36,38 @@ module.exports = {
 	],
 	"/backend/": [
 		{
-			title: "后端",
+			title: "node",
 			children: [
 				{
-					title: "node",
+					title: "Buffer 缓存区",
 				},
-			],
+				{
+					title: "fs 文件系统模块",
+					children: [
+						{
+							title: "读取文件",
+						},
+						{
+							title: "路径动态拼接问题 __dirname",
+						},
+						{
+							title: "其它操作",
+						},
+					],
+				},
+				{
+					title: "path 路径模块",
+				},
+				{
+					title: "http 模块",
+				},
+				{
+					title: "CommonJS 模块化规范",
+				},
+				{
+					title: "模块化",
+				},
+			],	
 		},
 	],
 	"/interview/": [
